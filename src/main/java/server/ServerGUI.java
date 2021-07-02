dc79373662d5949e4d17599b8d423ad8a5a7a2de
Looka997 <luca.marinelli997@gmail.com>
2021-07-02 21:48:06 +0200

    laid gui basic structure

---
+++ src/main/java/server/ServerGUI.java
@@ -0,0 +1,71 @@
+package server;
+
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ServerGUI extends JFrame {
+    final static boolean RIGHT_TO_LEFT = false;
+    private int port;
+
+    public ServerGUI(int port){
+        this.port = port;
+    }
+
+    public void addComponentsToPane(Container pane) {
+        if (RIGHT_TO_LEFT) {
+            pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        }
+        JButton openButton, closeButton;
+        pane.setPreferredSize(new Dimension(510, 450));
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weightx = 0.5;
+        JTextArea textArea = new JTextArea(10, 40);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(24, 30, 24, 30);
+        textArea.setEditable(false);
+        pane.add(textArea, c);
+
+        openButton = new JButton("Open");
+        c.gridy = 1;
+        c.gridx = 0;
+        c.ipady = 10;
+        c.ipadx = 7;
+        c.insets = new Insets(8, 24, 8, 24);
+        pane.add(openButton, c);
+
+
+        closeButton = new JButton("Close");
+        c.gridx = 0;
+        c.gridy = 2;
+        c.insets = new Insets(8, 24, 8, 24);
+        pane.add(closeButton, c);
+        closeButton.setEnabled(false);
+        pane.repaint();
+        pane.validate();
+
+    }
+
+    private void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Desktop client GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set up the content pane.
+        addComponentsToPane(frame.getContentPane());
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Runnable init = () -> {
+            new ServerGUI(8080).createAndShowGUI();
+        };
+        SwingUtilities.invokeLater(init);
+    }
+}
