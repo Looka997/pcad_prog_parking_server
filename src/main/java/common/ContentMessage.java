d58cdb644e4d657e0b7b0037541ee76d32b5734b
Looka997 <luca.marinelli997@gmail.com>
2021-09-25 14:49:58 +0200

    updates (and removes) common classes

--- src/main/java/common/ContentMessage.java
+++ src/main/java/common/ContentMessage.java
@@ -5,10 +5,10 @@ import java.util.Objects;
 
 public class ContentMessage {
 
-    private TipoRichiesta request;
-    private String plate;
-    private String brand;
-    private Instant date;
+    private final TipoRichiesta request;
+    private final String plate;
+    private final String brand;
+    private final Instant date;
 
     private void checkArguments(){
         Objects.requireNonNull(date);
@@ -28,7 +28,7 @@ public class ContentMessage {
     }
     @Override
     public String toString() {
-        return request.name() + "," + plate.toString() + "," + brand + "," + date.toString() + "\n";
+        return request.name() + "," + plate + "," + brand + "," + date.toString() + "\n";
     }
 
     public synchronized static ContentMessage fromString(String str){
